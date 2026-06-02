df123f278fe897cc001b2f89fc592273aff876ed
Nicolas Herrera Labra <nico.herreral@duocuc.cl>
2026-06-02 18:47:17 -0400

    eliminacion de imports

--- pago/src/main/java/com/marketplace/pago/DTO/PagoResponseDTO.java
+++ pago/src/main/java/com/marketplace/pago/DTO/PagoResponseDTO.java
@@ -1,6 +1,5 @@
 package com.marketplace.pago.DTO;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- reporte/src/main/java/com/marketplace/reporte/DTO/ReporteResponseDTO.java
+++ reporte/src/main/java/com/marketplace/reporte/DTO/ReporteResponseDTO.java
@@ -1,6 +1,5 @@
 package com.marketplace.reporte.DTO;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
