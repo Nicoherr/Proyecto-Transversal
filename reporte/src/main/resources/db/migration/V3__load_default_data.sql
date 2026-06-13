e9bf63a30666989a0e108de0b316056f847d50f7
Nicolas Herrera Labra <nico.herreral@duocuc.cl>
2026-06-12 21:29:58 -0400

    correcion de errores con claudio

---
+++ reporte/src/main/resources/db/migration/V3__load_default_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO reporte (tipo, descripcion, fecha, estado)
+VALUES ('Ventas mensuales', 'Reporte generado para visualizar ventas mensuales para los fines que se estimen convenientes', '2026-06-01', 1);
